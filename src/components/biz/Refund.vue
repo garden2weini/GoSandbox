ac786fe48560a19a98d03b29ef944a7bc7073af0
Merlin <moses7625@aliyun.com>
2019-12-28 14:38:57 +0800

    尝试解决问题：Cannot read property 'refundRate' of null"

--- src/components/biz/Refund.vue
+++ src/components/biz/Refund.vue
@@ -3,13 +3,13 @@
         <sub-topic :msg="msg" :hasBorder="hasBorder" />
         <div class="row">
             <div class="col-md-6 col-lg-6 text-center" >
-                退款占比：{{data.refundRate}}%
+                退款占比：{{data1.refundRate}}%
             </div>
             <div class="col-md-3 col-lg-3 text-left" >
-                退款：{{data.refundCnt}}
+                退款：{{data1.refundCnt}}
             </div>
             <div class="col-md-3 col-lg-3 text-left" >
-                ¥{{data.refundAmt}}
+                ¥{{data1.refundAmt}}
             </div>
         </div>
         <div class="row">
@@ -40,8 +40,15 @@
                     colors: ['#E8A61F', '#E67C26', '#0CD8A7', '#ACD8A7'],
                     values: [53.1, 40, 23, 10]
                 },
+				data1: {
+					refundRate:0,
+					refundCnt: 0,
+					refundAmt: 0},
             }
-        }
+        },
+		beforeUpdate() { // 当data更新时触发
+		    this.data1 = this.data;
+		}
     }
 </script>
 
